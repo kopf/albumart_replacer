4467b9bea1fa009bc78fdbff8ea6e07d5c8c16f2
Aengus Walton <ventolin@gmail.com>
2012-02-04 12:49:17 +0100

    Completely refactored fallback process, fixed major bug caused by fallback process being originally an afterthought.

--- main.py
+++ main.py
@@ -19,60 +19,64 @@ def search(filepath):
 
 def execute():
     mp3path = sys.argv[len(sys.argv) - 1] # c:\album\01 - song.mp3
-    path = os.path.dirname(mp3path) # c:\album
-    orig_img_path = None # c:\album\folder.jpg
+    dirpath = os.path.dirname(mp3path) # c:\album
+    orig_img_fpath = None # c:\album\folder.jpg
     filename = None # folder.jpg
     pixels = None # number of pixels in original image
     
     for file in FILENAMES:
-        if os.path.exists(os.path.join(path, file)):
+        if os.path.exists(os.path.join(dirpath, file)):
             filename = file
-            orig_img_path = os.path.join(path, file)
-            pixels = Image.open(orig_img_path).size
+            orig_img_fpath = os.path.join(dirpath, file)
+            pixels = Image.open(orig_img_fpath).size
             pixels = pixels[0]*pixels[1]
             break
     
-    if orig_img_path:
+    if orig_img_fpath:
         print 'Searching for larger images...'
-        html = search(orig_img_path)
+        html = search(orig_img_fpath)
         
         print 'Processing search results...'
         soup = BeautifulSoup(html)
-        div = soup.find('div', {'class':'result-match clearfix'})
-        if not div:
+        results = soup.findAll('div', {'class':'result-match clearfix'})
+        
+        if not results:
             print '''Didn't find any matching images. Aborting.'''
             return
-        pixels_span = div.find('div', {'class': 'result-match-image'}).findAll('span')[-1]
-        regexp = re.search('''(\d+)x(\d+)''', str(pixels_span))
-        found_pixels = int(regexp.group(0).split('x')[0]) * int(regexp.group(0).split('x')[1])
-        if found_pixels <= pixels:
-            print '''Only found images smaller than your current one. Aborting.'''
-            return
+        
         urls = []
-        for block in div.findAll('div', {'class': 'location-match'}):
-            try:
-                link = urls.append(block.find('a')['href'])
-                if not link in urls:
-                    urls.append(link)
-            except Exception, e:
+        for result in results:
+            pixels_span = result.find('div', {'class': 'result-match-image'}).findAll('span')[-1]
+            regexp = re.search('''(\d+)x(\d+)''', str(pixels_span))
+            found_pixels = int(regexp.group(0).split('x')[0]) * int(regexp.group(0).split('x')[1])
+            if found_pixels <= pixels:
                 pass
-        
-        print 'Downloading new image...'
-        os.rename(orig_img_path, os.path.join(path, u'Backup of ' + filename))
-        
-        # fall-back mechanism:
-        success = False
-        i = 0
-        while not success and i < len(urls):
-            try:
-                urlretrieve(urls[i], orig_img_path)
-                success = True
-            except Exception, e:
-                i += 1
-        if success:
-            print 'Done!'
+            else:
+                for block in result.findAll('div', {'class': 'location-match'}):
+                    try:
+                        urls.append(block.find('a')['href'])
+                    except Exception, e:
+                        pass
+        if not urls:
+            print 'No similar images found. Exiting...'
+            return
         else:
-            print 'Exhausted URL list. Download failed.'
+            print 'Downloading new image...'
+            os.rename(orig_img_fpath, os.path.join(dirpath, u'Backup of ' + filename))
+            
+            # fall-back mechanism:
+            success = False
+            i = 0
+            while not success and i < len(urls):
+                try:
+                    urlretrieve(urls[i], orig_img_fpath)
+                    success = True
+                except Exception, e:
+                    i += 1
+            if success:
+                print 'Done!'
+            else:
+                print 'Exhausted URL list. Download failed.'
     else:
         print 'File not found.'
 
@@ -81,3 +85,4 @@ if __name__ == '__main__':
         from lib import win32_unicode_argv
         sys.argv = win32_unicode_argv()
     execute()
+    
